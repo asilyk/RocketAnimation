dce9f57e0970e4d4430f996f61e364e28b7e7864
fabet <asilyk112@gmail.com>
2022-05-04 14:15:40 +0300

    Initial Commit

---
+++ RocketAnimation/RocketAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  RocketAnimationApp.swift
+//  RocketAnimation
+//
+//  Created by Alexander on 04.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct RocketAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
